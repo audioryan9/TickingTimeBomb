13cebcc79a324c39aa12b1d57247ec5d0533d7bd
audioryan9 <ryandsgn@outlook.com>
2019-10-09 16:42:59 +0000

    secnd

--- my-app/src/Layout of Bomb.js
+++ my-app/src/Layout of Bomb.js
@@ -1,5 +1,5 @@
 import React, { useState , useEffect } from 'react';
-import BombCount from "./TimeBomb"
+import BombCount from "./TimeBomb.js"
         // let kaBoomStack= "LayoutBomb"
 
 function LayoutBomb() {
@@ -15,7 +15,7 @@ function LayoutBomb() {
       <a class="mr-3" href="#">
         <img src="..." class="mr-3" alt="..." img/>
       </a>
-      <div id="kaBoom2" class="media-body">
+      <div id="kaBoom1" class="media-body">
         <h5 class="mt-0">{BombCount}</h5>
         Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
       </div>
@@ -24,7 +24,7 @@ function LayoutBomb() {
 </div>
 <div class="media">
   <img src="..." class="mr-6" alt="..."img/>
-  <div id="kaboom3" class="media-body">
+  <div id="kaBoom2" class="media-body">
     <h5 class="mt-3">Media heading</h5>
     Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
 
@@ -32,7 +32,7 @@ function LayoutBomb() {
       <a class="mr-3" href="#">
         <img src="..." class="mr-6" alt="..." img/>
       </a>
-      <div id="kaBoom4" class="media-body">
+      <div id="kaBoom2" class="media-body">
         <h5 class="mt-3">Media heading</h5>
         Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
       </div>
--- my-app/src/TimeBomb.js
+++ my-app/src/TimeBomb.js
@@ -1,21 +1,47 @@
 import React, { useState , useEffect } from 'react';
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCoffee } from '@fortawesome/free-solid-svg-icons'
+import timeBomb from './timeBomb.css'
 
 function BombCount() {
-  const [count, setCount] = useState(0);
+  const [count, setCount] = useState(500);
+
+  setTimeout(() => setCount(count - 1), 500);
 
   useEffect(() => {
-      setTimeout(() => setCount(count + 1), 1000);
   }, [count])
 
-  return (
-    <>
-        <div>
-            <p>You have this long until {count} kaboom</p>
-            <div onLoad={() => setCount(count + 1)}>
+            let myTimer= [" " , " ", " ", " "];
+            for (let i=0;i<myTimer.length;i++){
 
-            </div>
-        </div>
+}
+
+
+let one = count % 10;
+let two = Math.floor((count % 100) / 10);
+let three = Math.floor((count % 1000) / 100)
+
+return (
+    <>
+        <span class="fa-stack fa-lg">
+            <i class="fa fa-bomb fa-5x">{count}</i>
+            <i class="fa fa-frown icon-white"></i>
+        </span>
+        <span class="fa-stack fa-lg">
+            <i class="fa fa-bomb fa-5x">{three}</i>
+            <i class="fa fa-frown icon-white"></i>
+        </span>
+        <span class="fa-stack fa-lg">
+            <i class="fa fa-bomb fa-5x">{two}</i>
+            <i class="fa fa-frown icon-white"></i>
+        </span>
+        <span class="fa-stack fa-lg">
+            <i class="fa fa-bomb fa-5x">{one}</i>
+            <i class="fa fa-frown icon-white"></i>
+        </span>
+            <div onLoad={() => setCount(count + 1)}>
+            {/*<p>You have this long until  {count}  {three} {two} {one} kaboom</p>*/}
+    </div>
     </>
   );
 }
